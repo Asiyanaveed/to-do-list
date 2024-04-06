db02dd6f9334b7841f70ebff6bbadaaaa3c24de5
Asiya <latifnaved621@gmail.com>
2024-04-06 18:43:52 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todos =[];
+let condition = true;
+
+while(condition)
+{
+
+let answer= await inquirer.prompt(
+    [
+        {
+            name: 'firstQuestion',
+            type: 'input',
+            message: 'what would you like to add in your To do list?'
+
+        },
+        {
+            name: 'secondQuestion',
+            type: 'confirm',
+            message:'what would you like to add more in your To do list?',
+            defult: 'true'
+
+        }
+    ]
+);
+todos.push(answer.firstQuestion);
+console.log(todos);
+condition = answer.secondQuestion
+}
